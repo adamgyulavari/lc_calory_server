3c6df0d9c7b39ce9bf7fc7c2c490d1cc00fdb861
Adam Gyulavari <gyulavari.adam@gmail.com>
2016-01-04 02:04:36 +0100

    enh deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,8 @@ set :branch, "master"
 
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
+set :server_name, "li1416-200.members.linode.com"
+
 server '139.162.149.200', user: 'deployer', roles: %w{app db web}, primary: true
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
